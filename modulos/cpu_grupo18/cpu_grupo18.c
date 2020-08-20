a3826467c2674d89fed29a04aeb0e5f2ce9cdbd9
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-08-19 20:13:08 -0600

    generar datos de cpu en archivos en proc

--- modulos/cpu_grupo18/cpu_grupo18.c
+++ modulos/cpu_grupo18/cpu_grupo18.c
@@ -5,11 +5,6 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-MODULE_LICENSE("MIT");
-MODULE_AUTHOR("Mario Alvarado");
-MODULE_DESCRIPTION("Un monitor de CPU sencillo.");
-MODULE_VERSION("0.01");
-
 static int cpu_g18_main(struct seq_file *m, void *v)
 {
 	struct task_struct *procesos;
@@ -24,12 +19,14 @@ static int cpu_g18_main(struct seq_file *m, void *v)
 	return 0;
 }
 
+/*
 // Método auxiliar para la lectura
 static int abrir_proceso_cpu(struct inode *inode, struct file *file)
 {
 	return single_open(file, cpu_g18_main, NULL);
 }
-
+*/
+/*
 // Estructura para llevar el control del asunto.
 static struct file_operations opciones_de_archivo_procedimiento_cpu = {
 	.owner = THIS_MODULE,
@@ -38,16 +35,85 @@ static struct file_operations opciones_de_archivo_procedimiento_cpu = {
 	.llseek = seq_lseek,
 	.release = single_release,
 };
-
+*/
+/*
 int init_module(void)
 {
-	printk(KERN_INFO "Hola mundo, somos el grupo 18 y este es el monitor de CPU.\n");;
-	proc_create("cpu_grupo18", 0, NULL, &opciones_de_archivo_procedimiento_cpu);
-	return 0;
+	//printk(KERN_INFO "Hola mundo, somos el grupo 18 y este es el monitor de CPU.\n");;
+	//proc_create("cpu_grupo18", 0, NULL, &opciones_de_archivo_procedimiento_cpu);
+	//return 0;
 }
-
+*/
+/*
 void cleanup_module(void)
 {
-	remove_proc_entry("cpu_grupo18", NULL);
-	printk(KERN_INFO "Sayonara mundo, somos el grupo 18 y este fue el monitor de CPU.\n");
+	//remove_proc_entry("cpu_grupo18", NULL);
+	//printk(KERN_INFO "Sayonara mundo, somos el grupo 18 y este fue el monitor de CPU.\n");
 }
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+static int open_action(struct seq_file *m, void *v) {
+    struct task_struct *task;
+
+    seq_printf(m, "PID;NOMBRE_PROCESO\n");
+
+    for_each_process(task) {
+        if(task->mm) {
+            seq_printf(m,"%d;%s\n", task->pid, task->comm);
+        }
+    }
+
+    return 0;
+}
+*/
+
+/*
+ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp) {
+    return 0;
+}
+*/
+
+int open_proc_cpu(struct inode *inode, struct file *file) {
+    //return single_open(file, open_action, NULL);
+	return single_open(file, cpu_g18_main, NULL);
+}
+
+static struct file_operations proc_fops = {
+    read : seq_read,
+    write : write_proc,
+    open : open_proc_cpu,
+    release : single_release,
+    llseek : seq_lseek
+};
+
+static int __init cpu_grupo18_init(void) {
+    printk(KERN_INFO "Hola mundo, somos el grupo 18 y este es el monitor de CPU.\n");
+
+    struct proc_dir_entry *entry;
+    entry =  proc_create("cpu_grupo18_datos", 0777, NULL, &proc_fops);
+
+    if (!entry) {
+        return -1;
+    }
+    return 0;
+}
+
+static void __exit cpu_grupo18_exit(void) {
+    printk(KERN_INFO "Sayonara mundo, somos el grupo 18 y este fue el monitor de CPU.\n");
+    remove_proc_entry("cpu_grupo18_datos", NULL);
+}
+
+
+
+module_init(cpu_grupo18_init);
+module_exit(cpu_grupo18_exit);
+
+
+
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("Grupo 18");
+MODULE_DESCRIPTION("Un monitor de CPU sencillo.");
+MODULE_VERSION("0.01");
