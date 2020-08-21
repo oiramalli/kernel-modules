79d43987919f69dabf320143139715dcda0adde8
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-08-21 09:36:06 -0600

    if total_vm

--- modulos/cpu_grupo18/cpu_grupo18.c
+++ modulos/cpu_grupo18/cpu_grupo18.c
@@ -19,17 +19,21 @@
 static int cpu_g18_main(struct seq_file *m, void *v)
 {
     struct task_struct *procesos;
-	size_t process_counter = 0;
-	for_each_process(procesos)
-	{
-        unsigned long ram = procesos->mm->total_vm;
+    size_t process_counter = 0;
+    for_each_process(procesos)
+    {
+        unsigned long ram = 0;
+        if (!procesos->mm && !procesos->mm->total_vm)
+        {
+            ram = procesos->mm->total_vm;
+        }
         ram *= 2;
-		char *estado = procesos->state == -1 ? "Inejecutable" : procesos->state == 0 ? "Ejecutable" : "Detenido";
-		seq_printf(m, "PID: %d\n\tNombre: %s\n\tUsuario: %d\n\tEstado: %s(%ld)\n\tRAM: %ld\n\n", procesos->pid, procesos->comm, procesos->cred->euid,  estado, procesos->state, ram);
-		++process_counter;
-	}
-	seq_printf(m, "Número de procesos: %zu\n", process_counter);
-	return 0;
+        char *estado = procesos->state == -1 ? "Inejecutable" : procesos->state == 0 ? "Ejecutable" : "Detenido";
+        seq_printf(m, "PID: %d\n\tNombre: %s\n\tUsuario: %d\n\tEstado: %s(%ld)\n\tRAM: %ld\n\n", procesos->pid, procesos->comm, procesos->cred->euid, estado, procesos->state, ram);
+        ++process_counter;
+    }
+    seq_printf(m, "Número de procesos: %zu\n", process_counter);
+    return 0;
 }
 
 /*
