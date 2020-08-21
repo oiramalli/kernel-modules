568ab062817c644f5979d7c8171676abc0c6154f
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-08-20 22:26:10 -0600

    task size

--- modulos/cpu_grupo18/cpu_grupo18.c
+++ modulos/cpu_grupo18/cpu_grupo18.c
@@ -1,6 +1,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>		// pr_info
+#include <linux/sched.h>        // pr_info
 #include <linux/sched/signal.h> // for_each_process
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
@@ -16,15 +16,14 @@
 #include <linux/cred.h>
 #include <linux/mm_types.h>
 
-
 static int cpu_g18_main(struct seq_file *m, void *v)
 {
-	struct task_struct *procesos;
+    struct task_struct *procesos;
 	size_t process_counter = 0;
 	for_each_process(procesos)
 	{
 		char *estado = procesos->state == -1 ? "Inejecutable" : procesos->state == 0 ? "Ejecutable" : "Detenido";
-		seq_printf(m, "PID: %d\n\tNombre: %s\n\tUsuario: %d\n\tEstado: %s(%ld)\n\tRAM: %lld\n\n", procesos->pid, procesos->comm, procesos->cred->euid,  estado, procesos->state, procesos->acct_vm_mem1);
+		seq_printf(m, "PID: %d\n\tNombre: %s\n\tUsuario: %d\n\tEstado: %s(%ld)\n\tRAM: %ld\n\n", procesos->pid, procesos->comm, procesos->cred->euid,  estado, procesos->state, procesos->mm->task_size);
 		++process_counter;
 	}
 	seq_printf(m, "Número de procesos: %zu\n", process_counter);
@@ -82,15 +81,15 @@ static int open_action(struct seq_file *m, void *v) {
 }
 */
 
-
-ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp) {
+ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp)
+{
     return 0;
 }
 
-
-int open_proc(struct inode *inode, struct file *file) {
+int open_proc(struct inode *inode, struct file *file)
+{
     //return single_open(file, open_action, NULL);
-	return single_open(file, cpu_g18_main, NULL);
+    return single_open(file, cpu_g18_main, NULL);
 }
 
 static struct file_operations proc_fops = {
@@ -101,29 +100,28 @@ static struct file_operations proc_fops = {
     llseek : seq_lseek
 };
 
-static int __init cpu_grupo18_init(void) {
+static int __init cpu_grupo18_init(void)
+{
     printk(KERN_INFO "Hola mundo, somos el grupo 18 y este es el monitor de CPU.\n");
 
     struct proc_dir_entry *entry;
-    entry =  proc_create("cpu_grupo18", 0777, NULL, &proc_fops);
+    entry = proc_create("cpu_grupo18", 0777, NULL, &proc_fops);
 
-    if (!entry) {
+    if (!entry)
+    {
         return -1;
     }
     return 0;
 }
 
-static void __exit cpu_grupo18_exit(void) {
+static void __exit cpu_grupo18_exit(void)
+{
     printk(KERN_INFO "Sayonara mundo, somos el grupo 18 y este fue el monitor de CPU.\n");
     remove_proc_entry("cpu_grupo18", NULL);
 }
 
-
-
 module_init(cpu_grupo18_init);
 module_exit(cpu_grupo18_exit);
-
-
 
 MODULE_LICENSE("MIT");
 MODULE_AUTHOR("Grupo 18");
